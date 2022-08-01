31739b13c5f8e7b7fe807b628276e3fcd4be6f64
swathi271991 <68529911+swathi271991@users.noreply.github.com>
2022-08-01 14:17:33 +0530

    qw

--- src/PracticeJava/ArrayReverse.java
+++ src/PracticeJava/ArrayReverse.java
@@ -17,7 +17,7 @@ public class ArrayReverse {
 		for(int i=(a.length-1); i>=0; i--)
 		{
 			System.out.print(a[i]);
-			System.out.println("Hiiiiiiiiiiiiii");
+			System.out.println("Hiiiiiiiiiiiiii GELLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWw");
 		}
 		
 		
